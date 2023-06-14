192dfcd261f77e9204184e6d7cd9887e3d851268
GrandaCoder <camilo.granda@correounivalle.edu.co>
2023-06-14 09:20:39 -0500

    inicio de uso de use context en react

---
+++ src/components/TodosLoading/TodosLoading.js
@@ -0,0 +1,14 @@
+import './TodosLoading.css'
+
+function TodosLoading() {
+    return (
+        <div className='li-container-loading'>
+            <p className='loading--circle-check'></p>
+            <p className='loading-text'></p>
+        </div>
+    )
+}
+
+export {
+    TodosLoading
+}
--- src/components/app/AppUI.js
+++ src/components/app/AppUI.js
@@ -4,47 +4,51 @@ import { TodoSearch } from '../TodoSearch/TodoSearch';
 import { TodoList } from '../TodoList/TodoList';
 import { TodoItem } from '../TodoItem/TodoItem';
 import { CreateTodoButton } from '../CretateTodoButtom/CreateTodoButton';
+import { TodosLoading } from '../TodosLoading/TodosLoading';
+import { EmptyTodos } from '../EmptyTodos/EmptyTodos';
 
 
-function AppUI({searchValue,
-   setSearchValue, 
-   todos, 
-   completeTodo, 
-   deleteTodo,
-   todosFiltered, 
-   numberOfDoneTodos, 
-   totalTodos,
-   loading,
-   error}) {
-    return (
-        // react fragment
-        <> 
-          <TodoCounter completedTodos={numberOfDoneTodos} totalTodos={totalTodos} />
-          <TodoList>
-              {loading && <p>Cargando...</p>}
-              {error && <p>Hubo un error</p>}
-              {(!loading && todos.length === 0) && <p>No hay tareas</p>}
+function AppUI({
+  searchValue,
+  setSearchValue,
+  todos,
+  completeTodo,
+  deleteTodo,
+  todosFiltered,
+  numberOfDoneTodos,
+  totalTodos,
+  loading,
+  error
+}) {
+  return (
+    // react fragment
+    <>
+      <TodoCounter completedTodos={numberOfDoneTodos} totalTodos={totalTodos} />
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
+      <TodoList>
+        {loading && <><TodosLoading /> <TodosLoading /><TodosLoading /> </>}
+        {error && <p>Hubo un error</p>}
+        {(!loading && todos.length === 0) && <EmptyTodos />}
 
-            <TodoSearch
-              searchValue={searchValue}
-              setSearchValue={setSearchValue}
-            />
-            {todosFiltered.map(todo => (
-              <TodoItem
-                key={todo.id}
-                text={todo.text}
-                done={todo.done}
-                // se le pasa una funcion que cuando se le hace click se ejecuta
-                onComplete={() => completeTodo(todo.id)}
-                onDelete={() => deleteTodo(todo.id)}
-              />
-            ))}
-          </TodoList>
-          <CreateTodoButton />
-        </>
-      );
+        {todosFiltered.map(todo => (
+          <TodoItem
+            key={todo.id}
+            text={todo.text}
+            done={todo.done}
+            // se le pasa una funcion que cuando se le hace click se ejecuta
+            onComplete={() => completeTodo(todo.id)}
+            onDelete={() => deleteTodo(todo.id)}
+          />
+        ))}
+      </TodoList>
+      <CreateTodoButton />
+    </>
+  );
 }
 
-export{
-    AppUI
+export {
+  AppUI
 }
